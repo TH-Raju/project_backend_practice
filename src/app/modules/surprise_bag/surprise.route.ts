2821afc3537ac237b89addc0978e68e918e07d56
TH-Raju <rajukhan894200@gmail.com>
2024-03-05 03:21:25 +0600

    [Debug] zod error

--- src/app/modules/surprise_bag/surprise.route.ts
+++ src/app/modules/surprise_bag/surprise.route.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import { SurpriseBagController } from './surprise.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { SurpriseValidation } from './surprise.validation';
 
 const router = express.Router();
 
-router.post('/create-surprise', SurpriseBagController.createSurprise);
+router.post(
+  '/create-surprise',
+  validateRequest(SurpriseValidation.surpriseZodSchema),
+  SurpriseBagController.createSurprise,
+);
 
 export const surpriseBagRoutes = router;
