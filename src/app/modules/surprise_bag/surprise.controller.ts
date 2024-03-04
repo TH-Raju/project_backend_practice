d604096f902f341b6b04b67a87fbf57c5e7dcba9
TH-Raju <rajukhan894200@gmail.com>
2024-03-05 03:32:29 +0600

    [Done] surprise crud operation

--- src/app/modules/surprise_bag/surprise.controller.ts
+++ src/app/modules/surprise_bag/surprise.controller.ts
@@ -16,6 +16,54 @@ const createSurprise = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const getAllSurprise = catchAsync(async (req: Request, res: Response) => {
+  const result = await SurpriseBagService.getAllSurpriseBagsFromDB();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Surprise Bags fetched successfully',
+    data: result,
+  });
+});
+
+const updateSurpriseBag = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const surprise = req.body;
+  const result = await SurpriseBagService.updateSurpriseBagFromDB(id, surprise);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Surprise Bag updated successfully',
+    data: result,
+  });
+});
+
+const getSingleSurprise = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await SurpriseBagService.getSingleSurpriseBagFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Surprise Bag fetched successfully',
+    data: result,
+  });
+});
+
+const deleteSurpriseBag = catchAsync(async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const result = await SurpriseBagService.deleteSurpriseBagFromDB(id);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Surprise Bag deleted successfully',
+    data: result,
+  });
+});
+
 export const SurpriseBagController = {
   createSurprise,
+  getAllSurprise,
+  updateSurpriseBag,
+  getSingleSurprise,
+  deleteSurpriseBag,
 };
